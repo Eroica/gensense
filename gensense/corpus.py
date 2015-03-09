bc6689af1f2d967ef81e06792a13ff42b3ad2e3a
Seba <lseba@icloud.com>
2015-03-09 17:06:06 +0100

    init evaluation corpus

--- gensense/corpus.py
+++ gensense/corpus.py
@@ -10,9 +10,19 @@ class EvaluationCorpus(list):
                 self.append([" ".join(eval_line[0:2]),
                              " ".join(eval_line[2:4]),
                              eval_line[-1]])
-                # self.append([x[4:] for x in line])
 
         self.evaluation_file = "share/share.html"
+
+        # Since ... original evaluation used numbers from 1 to 7 to rate
+        # the similarity between two sentences, this list is used to
+        # map values from 0.0 to 1.0 to values from 1 to 7.
+        # The same list procedurally generated could look like this:
+        #
+        #   self.grads = map(lambda x: 1 - x * (1.0/7),
+        #                    [x for x in range(0, 8)])
+        #
+        self.grads = [1.0, 6.0/7, 5.0/7, 4.0/7,
+                      3.0/7, 2.0/7, 1.0/7, 0.0]
 
     def iterator(self):
         with open(self.evaluation_file) as f:
