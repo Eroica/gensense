61020cf05b519d0e8073529aabc6764f7a7ede43
Seba <lseba@icloud.com>
2015-03-11 16:18:58 +0100

    update docstrings

--- gensense/corpus.py
+++ gensense/corpus.py
@@ -1,30 +1,27 @@
+"""This module is used to
+
+"""
 
 class EvaluationCorpus(list):
+    """
+
+
+    """
+
     def __init__(self):
+        """
+        """
 
         super(EvaluationCorpus, self).__init__()
+
+        self.evaluation_grades = []
 
         with open("share/share.html") as f:
             for line in f:
                 eval_line = line.split()[3:]
+                self.evaluation_grades = eval_line[-1]
+
                 self.append([" ".join(eval_line[0:2]),
-                             " ".join(eval_line[2:4]),
-                             eval_line[-1]])
+                             " ".join(eval_line[2:4])])
 
         self.evaluation_file = "share/share.html"
-
-        # Since ... original evaluation used numbers from 1 to 7 to rate
-        # the similarity between two sentences, this list is used to
-        # map values from 0.0 to 1.0 to values from 1 to 7.
-        # The same list procedurally generated could look like this:
-        #
-        #   self.grads = map(lambda x: 1 - x * (1.0/7),
-        #                    [x for x in range(0, 8)])
-        #
-        self.grads = [1.0, 6.0/7, 5.0/7, 4.0/7,
-                      3.0/7, 2.0/7, 1.0/7, 0.0]
-
-    def iterator(self):
-        with open(self.evaluation_file) as f:
-            for line in f:
-                yield line
--- gensense/sentence.py
+++ gensense/sentence.py
@@ -1,14 +1,18 @@
+"""
+
+"""
+
 import gensim
 import numpy
 import scipy.spatial
 import math
+
 from copy import deepcopy
 from collections import OrderedDict
-import itertools
 
 DEBUG = True
 STOP_LIST = set("for a an of the and to in".split())
-
+MODEL = gensim.models.word2vec.Word2Vec.load("share/vectors.bin")
 
 class Sentence(OrderedDict):
     """`Sentence' is an object representing a sentence. It is an
@@ -17,32 +21,30 @@ class Sentence(OrderedDict):
 
     """
 
-    def __init__(self, sentence, model, strict=False):
+    def __init__(self, sentence, model=MODEL, strict=False):
         """Creates a `Sentence' object. A sentence is represented as an
         OrderedDict in which every word is a key with the corresponding
         vector space representation of that word as a value.
 
-        For instance:
+        :Example:
 
-        [   "this":    [0, 0, 1]),
-            "is":      [0, 1, 0]),
-            "an":      [1, 0, 0]),
-            "example": [1, 1, 1])  ]
+        >>> s = gensense.sentence.Sentence("this is an example")
+        >>> s
+        Sentence([('this', [1, 1, 1]), ('is', [1, 0, 1]), ('an', [0, 0, 1]), ('example', [0, 1, 0])])
 
-        @params:
-            sentence : str
-                A string
+        :params: sentence: A sentence string
+        :type: sentence: str
 
-            model : gensim.models.word2vec.Word2Vec
-                A model created by gensim.models.word2vec.Word2Vec()
+        :params: model:
+            A model created by gensim.models.word2vec.Word2Vec()
+        :type: model : gensim.models.word2vec.Word2Vec
 
-            strict=False : boolean
-                (Optional) specifies whether words not found in `model'
-                should be ignored. If True, Sentence creation will
-                abort.
+        :params: strict=False:
+            (Optional) specifies whether words not found in `model'
+            should be ignored. If True, Sentence creation will abort.
+        :type: strict=False: bool
 
-        @returns:
-            Sentence : Sentence
+        :rtype: Sentence
         """
 
         super(Sentence, self).__init__()
@@ -67,13 +69,8 @@ class Sentence(OrderedDict):
         self.clusterize()
 
     def __str__(self):
-        """Overrides the __str__() method to only return the sentence as
-        a list of words (thus ignoring the vector space representation).
-
-            Sentence([("this", [0, 0, 1]), ("is", [0, 1, 0]),
-             ("an", [1, 0, 0]), ("example", [1, 1, 1])])
-
-            => "this is an example"
+        """The __str__() method is overwritten to only return the
+        sentence's words (thus ignoring each word's vector).
         """
 
         return " ".join((x for x in self.keys()))
@@ -83,59 +80,34 @@ class Sentence(OrderedDict):
         `stop_list'. This method operates on this object's state and
         does not return anything.
 
-        @params:
-            stop_list : list
-                List of strings of words
-        @returns:
-            None
+        :params: stop_list: List of strings of words that can be removed
+        :type: stop_list: list
         """
+
         for word in self.keys():
             if word in stop_list:
                 self.pop(word)
                 print "`" + word + "' has been removed from the sentence."
 
-        self.updateSentenceVector()
-
-    def updateSentenceVector(self):
-        """
-        """
-
-        self.sentenceVector = reduce(lambda x, y: numpy.multiply(x,y),
-                                     [self[x] for x in self])
-
-    def sv_additive(self):
-
-        sv = reduce(lambda x, y: x + y, self.values())
-
-        return sv
-
-
-    def sv_multiplicative(self):
-
-        sv = reduce(lambda x, y: numpy.multiply(x, y),
-                    self.values())
-
-        return sv
-
-
-    def sv_kintsch(self):
-        pass
-
     def clusterize(self):
-        """
+        """Maps each word of a sentence to an appropriate cluster by
+        using a ``Chinese Restaurant Problem'' algorithm.
         """
 
+        # The first word immediately creates a first cluster
         first_pair = tuple(self.items()[0])
-
         self.clusters = [[first_pair[0]]]
         self.cluster_sums = [deepcopy(first_pair[1])]
 
+        # With a probability of 1/(n + len(clusters)), a new cluster is created
         rands = numpy.random.rand(len(self))
         pnew = 1.0
 
         for i, word in enumerate(self.items()[1:]):
             maxSim = float("inf")
 
+            # Calculates the similarity of `word' with each cluster, and saves
+            # the cluster with the highest similarity
             for j, c in enumerate(self.cluster_sums):
                 sim = abs(scipy.spatial.distance.cosine(word[1], c))
                 sim = float("inf") if math.isnan(sim) else sim
@@ -145,18 +117,77 @@ class Sentence(OrderedDict):
                     maxClusterIndex = int(j)
 
             if maxSim > pnew and rands[i+1] < pnew:
+                # Create a new cluster, and update the cluster vector
                 self.clusters.append([deepcopy(word[0])])
                 self.cluster_sums.append(deepcopy(word[1]))
                 pnew = 1.0 / (1 + len(self.clusters))
             else:
+                # Do not create a new cluster, but add `word' to an
+                # existing one, and update the cluster vector
                 self.clusters[maxClusterIndex].append(word[0])
                 self.cluster_sums[maxClusterIndex] += word[1]
 
 
-    def returnVectors(self):
-        return [w[1] for w in self]
+def sv_additive(sentence):
+    """Returns a sentence vector where sv_i is the sum of each i-th
+    component of every word vector.
 
-model = gensim.models.word2vec.Word2Vec.load("share/vectors.bin")
+    :rtype: float
+    """
 
-def similarity(sentence1, sentence2, ):
-    return 1 - scipy.spatial.distance.cosine(sentence1, sentence2)
+    return reduce(lambda x, y: x + y, sentence.values())
+
+
+def sv_multiplicative(sentence):
+    """Returns a sentence vector where sv_i is the product of all i-th
+    components of every word vector.
+
+    :rtype: float
+    """
+
+    return reduce(lambda x, y: numpy.multiply(x, y),
+                  sentence.values())
+
+def cosine_to_integer(grade):
+    """Since Michell's and Lapata's original evaluation used numbers
+    from 1 to 7 to grade sentence similarity, this method takes a
+    cosine similarity (a number between 0.0 and 1.0) and maps them
+    to either 1, 2, 3, 4, 5, 6, or 7.
+
+    :rtype: int
+
+    :Example:
+
+    >>> gensense.sentence.cosine_to_integer(0.45)
+    >>> 4.0
+    """
+
+    # grads = map(lambda x: x * (1.0/7), [x for x in range(0, 8)])
+    return int(math.ceil(grade * 7))
+
+
+def similarity(sentence1, sentence2, sv_function=sv_additive):
+    """Returns the cosine similarity between two sentences, calculated
+    by comparing their sentence vectors with each other.
+
+    This function requires a function that explains how to calculate
+    each sentence's sentence vector. By default, an additive sentence
+    vector is used, but any other function that returns a sentence
+    vector (e.g., by multiplying each component instead of adding them)
+    can be used as a 3rd argument.
+
+    :params: sentence1: A sentence
+    :type: sentence1: Sentence
+
+    :params: sentence2: Another sentence
+    :type: sentence2: Sentence
+
+    :params: sv_function: Function calculating sentence vector
+    :type: sv_function: function
+
+    :returns: Number between 0.0 and 1.0 (cosine similarity)
+    :rtype: float
+    """
+
+    return 1 - scipy.spatial.distance.cosine(sv_function(sentence1),
+                                             sv_function(sentence2))
